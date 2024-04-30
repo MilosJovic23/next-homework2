00d4679bd47d208d4e68ced6927e85867f19f0d7
MilosJovic23 <milos.mj992@gmail.com>
2024-04-30 14:40:31 +0200

    * changes to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,28 @@ import styles from "./page.module.css";
 
 export default async function Home() {
 
-  const response = await fetch('https://dummyjson.com/products');
+  const response = await fetch('https://dummyjson.com/products?limit=9');
+  const data = await response.json();
+  console.log(data)
 
   return (
-   <p>test</p>
+      <>
+
+        <div className="productWrapper">
+          {
+            data.products.map((product)=>{
+              return <div className="singleProduct">
+                <h2>{product.title}</h2>
+                <p>{product.description}</p>
+                <b>{product.price}&euro;</b>
+                <p>{product.brand}</p>
+              </div>
+
+            })
+          }
+        </div>
+
+      </>
+
   );
 }
