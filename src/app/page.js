21af6c2d65714777bd643d3a91bd5d3abce38de3
MilosJovic23 <milos.mj992@gmail.com>
2024-04-30 14:25:43 +0200

    * changes to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+
+  const response = await fetch('https://dummyjson.com/products');
+
   return (
    <p>test</p>
   );
