c8f7689238163c647565d19a4f0cbc85dec567df
MilosJovic23 <milos.mj992@gmail.com>
2024-05-02 11:12:00 +0200

    * small changes to products\[slug]

--- src/app/products/[slug]/page.js
+++ src/app/products/[slug]/page.js
@@ -1,19 +1,19 @@
 
 
 
-export default async function Products({ params }){
+export default async function Products ({ params }){
 
     const response = await fetch('https://dummyjson.com/products/'+params.slug);
     const data = await response.json();
-    console.log(data);
 
     return(
         <>
             <div>
                 <h3>{data.title}</h3>
+                <p>{data.brand}</p>
                 <p>{data.description}</p>
-                <b>{data.price}</b>
-                <p>{data.description}</p>
+                <b>{data.price}&euro;</b>
+                <p>{data.category}</p>
             </div>
         </>
 
