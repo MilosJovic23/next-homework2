44602f93d31d5aaed45c1c95961423f1c59f7697
MilosJovic23 <milos.mj992@gmail.com>
2024-05-09 13:09:51 +0200

    * added new function for api call

--- services/productService.js
+++ services/productService.js
@@ -10,12 +10,17 @@ export default async function getAllProducts(limit=9){
     });
     return await response.json();
 
-}
+};
 
 export async function getProductById(productId){
     const response = await fetch(process.env.PRODUCTS_API_URL+'/products/'+productId);
     if ( response.status === 404 ){
         return false;
     }
+    return await response.json();
+}
+
+export async function searchProducts(searchTerm){
+    const response = await fetch(`https://dummyjson.com/products/search?q=${searchTerm}`);
     return await response.json();
 }
