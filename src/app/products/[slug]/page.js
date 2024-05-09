2fbdf664eb015b59cbd9d475fa061cba4df34353
MilosJovic23 <milos.mj992@gmail.com>
2024-05-09 14:38:32 +0200

    * added Search component

--- src/app/products/[slug]/page.js
+++ src/app/products/[slug]/page.js
@@ -1,4 +1,5 @@
 import getAllProducts, { getProductById } from "../../../../services/productService";
+import Search from "@/app/SearchByTitle";
 
 
 export default async function Products ({ params }){
@@ -10,7 +11,7 @@ export default async function Products ({ params }){
         return <h1>Ova stranice ne postoji</h1>
     }
     return<>
-
+            <Search/>
             <div>
                 <h3>{data.title}</h3>
                 <p>{data.brand}</p>
