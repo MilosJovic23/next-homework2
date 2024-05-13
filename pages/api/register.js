35f1443c5ef2ce0ec8c4840f0a1dd5b810556236
MilosJovic23 <milos.mj992@gmail.com>
2024-05-13 17:47:49 +0200

    * cleared console.log

--- pages/api/register.js
+++ pages/api/register.js
@@ -5,7 +5,7 @@ export default function handler(req,res){
 
 
     addProductAPI(req.body.title,req.body.price,req.body.description)
-    console.log(req.body)
+
 
     res.status(200).json({
         message:"success"
