e409d4e6ab32ace6f620b15dbedb9af0d0d4a85e
MilosJovic23 <milos.mj992@gmail.com>
2024-05-13 17:46:00 +0200

    * added fetch function for add product

--- services/productService.js
+++ services/productService.js
@@ -24,3 +24,17 @@ export async function searchProductByQuery(query){
     const response = await fetch(process.env.PRODUCTS_API_URL+`/products/search?q=${query}`);
     return await response.json();
 }
+
+export  function addProductAPI( name,price,description){
+    const res =fetch(process.env.PRODUCTS_API_URL+'/products/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            title: name,
+            price: price,
+            description:description
+        })
+    })
+        .then(res=>res.json())
+
+}
