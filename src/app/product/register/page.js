0298cdcc7cde422c70d3bb72c2673059f898f943
MilosJovic23 <milos.mj992@gmail.com>
2024-05-13 17:44:46 +0200

    * added routh product/register

---
+++ src/app/product/register/page.js
@@ -0,0 +1,38 @@
+"use client"
+import {useState} from "react";
+import {addProductAPI} from "../../../../services/productService";
+
+
+export default function Register(){
+
+    const [ProductName,setProductName]=useState("")
+    const [price,setPrice]=useState("")
+    const [description,setDescription]=useState("")
+
+
+    const createProduct=()=>{
+       fetch("/api/register",{
+           method: 'POST',
+           headers: { 'Content-Type': 'application/json'
+           },
+           body:JSON.stringify({
+               title:ProductName,
+               price:price,
+               description:description
+           })
+       })
+
+    }
+    return <>
+
+        <form>
+            <input type="text" placeholder="ime proizvoda" onChange={e => setProductName(e.currentTarget.value)}/>
+            <input type="number" placeholder="cena proizvoda" onChange={e => setPrice(e.currentTarget.value)}/>
+            <input type="text" placeholder="opis proizvoda" onChange={e => setDescription(e.currentTarget.value)}/>
+            <button type="button" onClick={ createProduct }>dodaj proizvod</button>
+        </form>
+        {/*<p>{data.id}</p>*/}
+        {/*<p>{data.title}</p>*/}
+        {/*<p>{data.description}</p>*/}
+    </>
+}
