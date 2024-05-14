509273a01e782cbde113bd3ba7936ae63496d231
MilosJovic23 <milos.mj992@gmail.com>
2024-05-14 14:55:34 +0200

    * cleared unused code

--- pages/api/register.js
+++ pages/api/register.js
@@ -3,9 +3,7 @@ import {addProductAPI, searchProductByQuery} from "../../services/productService
 
 export default function handler(req,res){
 
-
     addProductAPI(req.body.title,req.body.price,req.body.description)
-
 
     res.status(200).json({
         message:"success"
