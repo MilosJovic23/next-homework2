9bc8d62e3f3b0093abc290ff1e98e7aa5b9fd7b7
MilosJovic23 <milos.mj992@gmail.com>
2024-05-21 18:04:10 +0200

    * small changes

--- src/app/user/login/page.js
+++ src/app/user/login/page.js
@@ -1,34 +1,46 @@
 
 "use client"
 
+import useSWR from "swr";
+import {useState} from "react";
+
 export default function Login(){
 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
 
-
-
-    const userInfoPostApi=()=>{
-        fetch("/api/auth_user",{
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json'
-            },
-            body:JSON.stringify({
-                username: 'nameTest',
-                password: 'passTest',
-                expiresInMinutes: 30
+    const handleLogin = async ()=>{
+        try {
+            const response = await fetch('/api/auth_user', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                })
             })
-        })
+        } catch (error) {
+            console.error('Login failed:', error);
+        }
 
     }
-
-
-    // const fetcher =  (url) => fetch(url).then(res => res.json());
-    // const { data: fetchData, error } = useSWR(`http://localhost:3000/api/auth_user`, fetcher);
-    // console.log(fetchData,"works")
-    return<>
+    return <>
 
         <div>
-            <button onClick={ userInfoPostApi }>dodaj info</button>
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleLogin}>Login</button>
         </div>
 
     </>
