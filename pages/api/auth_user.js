b4f6ef8902df67c86610a6f4aa1256efcaf04745
MilosJovic23 <milos.mj992@gmail.com>
2024-05-21 18:03:53 +0200

    * small changes

--- pages/api/auth_user.js
+++ pages/api/auth_user.js
@@ -1,9 +1,21 @@
-import {apiAuthentication} from "../../services/productService";
+export default async function handler(req, res) {
+
+    fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+
+            username: 'kminchelle',
+            password: '0lelplR',
+            expiresInMins: 30, // optional, defaults to 60
+        })
+    })
+        .then(res => res.json())
+        .then(console.log);
 
 
-    export default function handler(req,res){
+    res.status(200).json({
+        message:"success"
+    })
 
-        apiAuthentication(req.body.username,req.body.password)
-
-    res.status(200).json({message:"works"})
 }
