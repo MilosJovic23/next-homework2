0caee58aea4d67f8c937c6545ca645a0c39d12ac
MilosJovic23 <milos.mj992@gmail.com>
2024-05-22 13:31:13 +0200

    * small changes

--- pages/api/auth_user.js
+++ pages/api/auth_user.js
@@ -1,21 +1,24 @@
 export default async function handler(req, res) {
+    try {
+        const response = await fetch('https://dummyjson.com/auth/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                username: req.body.username,
+                password: req.body.password,
+                expiresInMins: 30
+            })
+        });
 
-    fetch('https://dummyjson.com/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
+        const data = await response.json();
 
-            username: 'kminchelle',
-            password: '0lelplR',
-            expiresInMins: 30, // optional, defaults to 60
-        })
-    })
-        .then(res => res.json())
-        .then(console.log);
+        console.log(req.body);
+        console.log(data)
 
-
-    res.status(200).json({
-        message:"success"
-    })
-
+        res.status(200).json({
+            message: "success"
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
