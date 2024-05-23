ed3b2ef58881bc96265d65bec1d56dd26fec7159
MilosJovic23 <milos.mj992@gmail.com>
2024-05-23 18:14:23 +0200

    * small changes

--- pages/api/auth_user.js
+++ pages/api/auth_user.js
@@ -4,16 +4,14 @@ export default async function handler(req, res) {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                username: req.body.username,
-                password: req.body.password,
+                username:'kminchelle',
+                password:  '0lelplR',
                 expiresInMins: 30
             })
         });
 
         const data = await response.json();
 
-        console.log(req.body);
-        console.log(data)
 
         res.status(200).json({
             message: "success"
