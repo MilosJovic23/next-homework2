99860457fd1ff0b066f76300bd8d5e9d73325004
MilosJovic23 <milos.mj992@gmail.com>
2024-05-24 19:38:53 +0200

    * small changes

--- pages/api/auth_user.js
+++ pages/api/auth_user.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    try {
+
         const response = await fetch('https://dummyjson.com/auth/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -12,11 +12,10 @@ export default async function handler(req, res) {
 
         const data = await response.json();
 
-
+        console.log(data)
         res.status(200).json({
             message: "success"
         });
-    } catch (error) {
-        console.error('Error:', error);
-    }
+
+
 }
