1e1577db129933add756782e157c52c5d218651f
MilosJovic23 <milos.mj992@gmail.com>
2024-06-03 15:59:25 +0200

    * firebase install

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -6,12 +6,7 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDLiwj9RhO7vGcqM8GRmVsDpde7aAqORk4",
-    authDomain: "fir-next-6ec67.firebaseapp.com",
-    projectId: "fir-next-6ec67",
-    storageBucket: "fir-next-6ec67.appspot.com",
-    messagingSenderId: "597107893872",
-    appId: "1:597107893872:web:a036622399dce4789e36b5"
+
 };
 
 
