c0bbcbbfe0455d2d8ea5c1f580a829089d66be74
MilosJovic23 <milos.mj992@gmail.com>
2024-06-04 19:10:50 +0200

    * firebase setup

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,7 +1,8 @@
 
-
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth"
 
 
 
