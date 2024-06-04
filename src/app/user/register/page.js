d72b6d2df55cfa63dedc931bb5f0bf5196a9fdba
MilosJovic23 <milos.mj992@gmail.com>
2024-06-04 19:11:01 +0200

    * firebase setup

--- src/app/user/register/page.js
+++ src/app/user/register/page.js
@@ -1,13 +1,18 @@
 
 "use client"
 
+import { auth } from "../firebase"
+
+import { createUserWithEmailAndPassword } from "firebase/auth"
 
 export default function Register() {
 
 
     const register = (e)=>{
+
+        const res = await createUserWithEmailAndPassword()
         e.preventDefault()
-        console.log("x");
+
     }
 
     return <>
