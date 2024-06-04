a983af13ef70a28348b1f0084b21240046e4c411
MilosJovic23 <milos.mj992@gmail.com>
2024-06-04 21:21:59 +0200

    * firebase setup

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,19 +1,23 @@
 
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
 
+
+
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+import { initializeApp } from "firebase/app";
+
+import { getAuth } from 'firebase/compat/auth'
 
 const firebaseConfig = {
 
 };
 
-
 const app = initializeApp(firebaseConfig);
 
 
 
-export const auth = getAuth(app);
+export const auth = getAuth(app)
+console.log(auth)
 
 export default app;
