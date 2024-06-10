1082a1d8362459facf31451695d3f52f9da6334c
MilosJovic23 <milos.mj992@gmail.com>
2024-06-10 13:46:21 +0200

    * firebase setup

--- src/app/user/register/page.js
+++ src/app/user/register/page.js
@@ -3,18 +3,29 @@
 
 import { auth } from "@/app/firebase"
 import { createUserWithEmailAndPassword } from "firebase/auth"
-import {useState} from "react";
+import  { useState } from "react";
 
 export default function Register() {
 
     const [email,setEmail]=useState("");
     const [password,setPassword]=useState("");
 
+
     const register = async (e)=>{
 
         e.preventDefault()
-        const res = await createUserWithEmailAndPassword(auth,email,password)
+        const res = await createUserWithEmailAndPassword( auth,email,password );
+        console.log(res)
+        localStorage.setItem("userToken",res.user.accessToken)
 
+    }
+
+    const user = auth.currentUser;
+    const userToken = localStorage.getItem("userToken")
+    if ( user.accessToken === userToken ) {
+        console.log("user is logged in")
+    } else {
+        console.log("user is not logged in.")
     }
 
     return <>
