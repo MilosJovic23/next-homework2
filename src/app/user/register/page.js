4256d6c80f0dcf57095d7686fefb294160fbb099
MilosJovic23 <milos.mj992@gmail.com>
2024-06-10 12:38:24 +0200

    * firebase setup

--- src/app/user/register/page.js
+++ src/app/user/register/page.js
@@ -3,17 +3,36 @@
 
 import { auth } from "@/app/firebase"
 import { createUserWithEmailAndPassword } from "firebase/auth"
+import {useState} from "react";
+
+
 
 export default function Register() {
 
+
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+
     const register = async (e)=>{
 
-        const res = await createUserWithEmailAndPassword(auth,"email@email.com","asdaadsfe23")
         e.preventDefault()
+        const res = await createUserWithEmailAndPassword(auth,email,password)
+
 
     }
     return <>
-        <p>Hello World</p>
-        <button onClick={e=>register(e) }>register</button>
+        <input
+            type="text"
+            value={email}
+            placeholder="email"
+            onChange={e => setEmail(e.currentTarget.value)}
+        />
+        <input
+            type="password"
+            value={password}
+            placeholder="password"
+            onChange={e => setPassword(e.currentTarget.value)}
+        />
+        <button onClick={e => register(e)}>register</button>
     </>
 }
