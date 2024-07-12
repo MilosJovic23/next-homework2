ddf77db4d275b87986a2ce3d6c6c8251de67fe7d
MilosJovic23 <milos.mj992@gmail.com>
2024-07-12 08:57:56 +0200

    * firebase setup

--- src/app/context/authContext.js
+++ src/app/context/authContext.js
@@ -1,7 +1,7 @@
 import {useEffect, useState} from "react";
 import {auth} from "@/app/firebase"
 
-const AuthContext = React.createContext(null)
+const AuthContext = React.createContext(null);
 
 }
  const AuthProvider = ({children}) => {
